1d65612ba38b7747f5adf6afaf7f6a165bff0f35
salas <salas@salas.local>
2025-02-10 09:57:01 -0500

    se completo la interfaz grafica de ususario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        new frmEstadistica().setVisible(true);
+        new FrmEstadistica().setVisible(true);
     }
 }
--- src/frmEstadistica.java
+++ src/frmEstadistica.java
@@ -1,43 +1,85 @@
-import javax.swing.WindowConstants;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class frmEstadistica extends JFrame {
+import javax.swing.*;
 
-    public frmEstadistica(){
-        setSize (600,300);
-        setTitle("Estadistica");
+public class FrmEstadistica extends JFrame {
+
+    JTextField txtDato;
+    JList lstMuestra;
+
+    public FrmEstadistica() {
+
+        setSize(600, 300);
+        setTitle("Estadística");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        JLabel lbDato=new JLabel("dato");
-        lbDato.setBounds(10,10,100,25);
-        getContentPane().add(lbDato);
+        JLabel lblDato = new JLabel("Dato");
+        lblDato.setBounds(10, 10, 100, 25);
+        getContentPane().add(lblDato);
 
-        JTextField txDato=new JTextField("");
-        txDato.setBounds(80,10,100,25);
-        getContentPane().add(txDato);   
+        txtDato = new JTextField();
+        txtDato.setBounds(80, 10, 100, 25);
+        getContentPane().add(txtDato);
 
-        JLabel  lblDatos=new JLabel("La muestra");
-        lblDatos.setBounds(250,10,100,25);
+        JLabel lblDatos = new JLabel("Muestra");
+        lblDatos.setBounds(210, 10, 100, 25);
         getContentPane().add(lblDatos);
 
-        JButton btnAgregar=new JButton("Agregar");
-        btnAgregar.setBounds(80,40,100,25);
+        JButton btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(80, 40, 100, 25);
         getContentPane().add(btnAgregar);
 
-        JButton btnQuitar=new JButton("Quitar");
-        btnQuitar.setBounds(80,70,100,25);
+        JButton btnQuitar = new JButton("Quitar");
+        btnQuitar.setBounds(80, 70, 100, 25);
         getContentPane().add(btnQuitar);
 
-        JList lstMuestra=new JList();
-        lstMuestra.setBounds(250,40,100,150);
+        lstMuestra = new JList();
+        lstMuestra.setBounds(210, 40, 100, 150);
         getContentPane().add(lstMuestra);
 
+        //evento de la GUI (Interfaz grafica de usuario)
+
+        JButton btnEstadistica = new JButton("calcular");
+        btnEstadistica.setBounds(30,200,100,25);
+        getContentPane().add(btnEstadistica);
+
+        // eventos de la GUI
+        btnAgregar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                agregarDato();
+            }
+        });
+
+        btnQuitar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                quitarDato();
+            }
+        });
     }
+
+    // declarar el arreglo que almacenará los datos de la muestra
+    private double[] muestra = new double[1000];
+    private int totalDatos = -1;
+
+    private void agregarDato() {
+        double dato = Double.parseDouble(txtDato.getText());
+        totalDatos++;
+        muestra[totalDatos] = dato;
+        mostrarMuestra();
+    }
+
+    private void mostrarMuestra() {
+        String[] strMuestra = new String[totalDatos + 1];
+        for (int i = 0; i <= totalDatos; i++) {
+            strMuestra[i] = String.valueOf(muestra[i]);
+        }
+        lstMuestra.setListData(strMuestra);
+    }
+
+    private void quitarDato() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en QUITAR");
+    }
+
 }
-
-
